0add663f970586ce92ecf3435e0da1ad896d6ab5
mail2rishi432@gmail.com <mail2rishi432@gmail.com>
2024-09-02 11:35:06 +0000

    commit

--- models/payments.sql
+++
@@ -1,9 +0,0 @@
-{{config(materialized='table')}}
-
-with rishi as (
-    select ID,
-    FIRST_NAME,
-    LAST_NAME
-    from {{source('datafeed_shared_schema','STG_CUSTOMERS')}})
-
-    select * from rishi 
