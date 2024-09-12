04d8a297f5bc6e90d6901b42f1ebf110f9f87c2e
mail2rishi432@gmail.com <mail2rishi432@gmail.com>
2024-09-12 06:36:01 +0000

    commit

--- models/amount_rishikesh.sql
+++ models/amount_rishikesh.sql
@@ -1,8 +1,14 @@
 {{ config(materialized='table') }}
 
 with consumer as (
-   select sum(AMOUNT) as total_sum 
-   from {{source('datafeed_shared_schema','SPCOLUMN_MACRO')}})
+    select id,
+            order_id,
+            payment_method,
+            amount,
+            RISHIKESH,
+            ({{calculate_amount('amount','RISHIKESH')}}) as new_amount
+            from 
+            {{source('datafeed_shared_schema','SPCOLUMN_MACRO')}})
 
 select * from consumer 
 
